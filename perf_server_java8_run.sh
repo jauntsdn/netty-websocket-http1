dc8055bc7618dbc357f9322219e02a685584a15e
Maksym Ostroverkhov <m.ostroverkhov@gmail.com>
2023-02-08 11:38:36 +0200

    perf server: java17 support

--- perf_server_java8_run.sh
+++ perf_server_java8_run.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-export NETTY_WEBSOCKET_HTTP1_PERFTEST_SERVER_OPTS='--add-exports java.base/sun.security.x509=ALL-UNNAMED'
-
 cd netty-websocket-http1-perftest/build/install/netty-websocket-http1-perftest/bin && ./netty-websocket-http1-perftest-server
